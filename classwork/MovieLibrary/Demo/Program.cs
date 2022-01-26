8e8445d682db51c003174cafcf5d7e0b3bac390b
JohnLTCC <john.lobsinger@my.tccd.edu>
2022-01-25 21:27:39 -0600

    Variables and Types in Demo

---
+++ classwork/MovieLibrary/Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primitives
+            //Integrals
+            sbyte sbyteValue = 10;
+            short shortValue = 20;
+            int intValue = 62_543; //The compiler ignores underscores.
+            long longValue = 40L; //L to turn the literal to a long.
+
+            //Floats
+            float floatValue = 45.6F;
+            double doubleValue = 5678.115;
+            decimal payRate = 17.50M;
+
+            bool isSuccessful = true;
+            bool isFailing = false;
+
+            char letterGrade = 'F';
+            string name = "John";
+
+            int hoursWorked;
+
+            //Scope starts at the variable declaration until the end of the block.
+
+            //hoursWorked = 10;
+            intValue = hoursWorked;
+        }
+    }
+}
