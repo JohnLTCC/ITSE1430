47e6775ad200cd9ca0d4b380f92d55ac08469b99
JohnLTCC <john.lobsinger@my.tccd.edu>
2022-01-27 21:38:31 -0600

    Added functions

--- classwork/MovieLibrary/Demo/Program.cs
+++ classwork/MovieLibrary/Demo/Program.cs
@@ -13,10 +13,29 @@ namespace Demo
             int intValue = 62_543; //The compiler ignores underscores.
             long longValue = 40L; //L to turn the literal to a long.
 
+            /*
+             * Size:
+             * byte - 1
+             * short - 2
+             * int - 4
+             * long - 8
+             */
+
             //Floats
             float floatValue = 45.6F;
             double doubleValue = 5678.115;
             decimal payRate = 17.50M;
+
+            /*
+             * Size:
+             * flost - 4
+             * double - 8
+             * decimal - a lot
+             * Precision:
+             * float - 5-7
+             * double - 12-15
+             * decimal - 80
+             */
 
             bool isSuccessful = true;
             bool isFailing = false;
@@ -24,11 +43,13 @@ namespace Demo
             char letterGrade = 'F';
             string name = "John";
 
+            //This is wasteful
             int hoursWorked;
+            hoursWorked = 0;
 
             //Scope starts at the variable declaration until the end of the block.
 
-            //hoursWorked = 10;
+            //Definitely assigned
             intValue = hoursWorked;
         }
     }
--- classwork/MovieLibrary/MovieLib.ConsoleHost/Program.cs
+++ classwork/MovieLibrary/MovieLib.ConsoleHost/Program.cs
@@ -6,8 +6,79 @@ namespace MovieLib.ConsoleHost
     {
         static void Main ( string[] args )
         {
-            // Hi :)
-            Console.WriteLine("Hello World!");
+            char input = DisplayMenu();
+
+            //TODO: Handle input
+            if (input == 'A')
+                AddMovie();
+        }
+
+        private static void AddMovie ()
+        {
+            string title = ReadString("Enter a movie title: ", true);
+            int duration = ReadInt32("Enter duration in minutes (>=0): ", 0);
+            int releaseYear = ReadInt32("Enter release year: ", 1900);
+            string rating = ReadString("Enter a rating (e.g. PG, PG-13): ", true);
+            string genre = ReadString("Enter a genre (optional): ", false);
+            bool isColor;
+            string description = ReadString("Enter a description (optional): ", false);
+        }
+
+        private static int ReadInt32 ( string message, int minimumValue )
+        {
+            Console.Write(message);
+
+            string input = Console.ReadLine();
+
+            //TODO: Validate
+            int result = Int32.Parse(input);
+            if (result >= minimumValue)
+                return result;
+
+            return -1;
+        }
+
+        //Function naming rules
+        //Functions are verbs
+        //Functions are always Pascal cased
+        //Functions should do a single, logical thing. Name should describe the action.
+        private static string ReadString ( string message, bool required )
+        {
+            Console.WriteLine(message);
+
+            string input = Console.ReadLine();
+
+            //TODO: Validate input, if required
+
+            return input;
+        }
+
+        static char DisplayMenu()
+        {
+            Console.WriteLine("A)dd Movie");
+            Console.WriteLine("V)iew Movie");
+            Console.WriteLine("E)dit Movie");
+            Console.WriteLine("D)elete Movie");
+            Console.WriteLine("Q)uit");
+
+            string input = Console.ReadLine();
+
+            //Validate input
+            if (input == "A")
+                return 'A';
+            else if (input == "V")
+                return 'V';
+            else if (input == "E")
+                return 'E';
+            else if (input == "D")
+                return 'D';
+            else if (input == "Q")
+                return 'Q';
+            else
+            {
+                Console.WriteLine("Invalid Input");
+                return 'X';
+            };
         }
     }
 }
