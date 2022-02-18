376558d889169e3681b49e55e6269dea49567737
JohnLTCC <john.lobsinger@my.tccd.edu>
2022-02-17 19:46:11 -0600

    Started the project files

---
+++ labs/Lab2/JohnLob.AdventureGame.ConsoleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JohnLob.AdventureGame.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
