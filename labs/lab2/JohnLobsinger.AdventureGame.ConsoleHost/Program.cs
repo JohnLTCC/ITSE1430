93afad69a20dc82c898a9a579fe1674a09173963
John Lobsinger <john.lobsinger@my.tccd.edu>
2022-03-08 14:43:47 -0600

    Story 2 finished

---
+++ labs/lab2/JohnLobsinger.AdventureGame.ConsoleHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace JohnLobsinger.AdventureGame.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplaySetting();
+
+        }
+
+        static void DisplaySetting()
+        {
+            Console.WriteLine("Cellar Crawl - a game made by John Lobsinger for ITSE1430 spring 2022\n");
+            Console.WriteLine("Greetings adventurer, while on a quest to track the disapearance of some villagers you arive to find it deserted");
+            Console.WriteLine("While searching for the missing villagers you find scratch marks leading to a celler in the chiefs house.");
+            Console.WriteLine("As you open the hatch and peer into a pitch black haze, you light your torch and venture down.");
+        }
+    }
+}
