c1ecf0025b99c42603baae5027442e5906e48aa5
JohnLTCC <john.lobsinger@my.tccd.edu>
2022-03-08 20:03:46 -0600

    Added some properties

---
+++ labs/Lab2/JohnLobsinger.AdventureGame/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JohnLobsinger.AdventureGame
+{
+    public class Room
+    {
+        /// <summary>
+        /// Reads and writes the name of the room
+        /// </summary>
+        public string Name
+        {
+            get { return _name ?? ""; }
+            set { _name = value; }
+        }
+        private string _name;
+        /// <summary>
+        /// Reads and writes the decription of the room
+        /// </summary>
+        public string Description
+        {
+            get { return _description ?? ""; }
+            set { _description = value; }
+        }
+        private string _description;
+    }
+}
