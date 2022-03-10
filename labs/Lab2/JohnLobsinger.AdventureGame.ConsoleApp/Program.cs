ceb3c78248acfc857571fc21074625f22b1c4f74
John Lobsinger <john.lobsinger@my.tccd.edu>
2022-03-09 20:05:18 -0600

    Finished

--- labs/Lab2/JohnLobsinger.AdventureGame.ConsoleApp/Program.cs
+++ labs/Lab2/JohnLobsinger.AdventureGame.ConsoleApp/Program.cs
@@ -4,17 +4,124 @@ namespace JohnLobsinger.AdventureGame.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            DisplaySetting();
-        }
-
-        static void DisplaySetting ()
+        static void Main ( string[] args )
         {
             Console.WriteLine("Cellar Crawl - a game made by John Lobsinger for ITSE1430 spring 2022\n");
-            Console.WriteLine("Greetings adventurer, while on a quest to track the disapearance of some villagers you arive to find it deserted");
-            Console.WriteLine("While searching for the missing villagers you find scratch marks leading to a celler in the chiefs house.");
-            Console.WriteLine("As you open the hatch and peer into a pitch black haze, you light your torch and venture down.");
+            s_world.DisplaySetting();
+
+            s_world.Initialize();
+            s_player.CurrentRoom = s_world.SouthRoomStart;
+
+            DisplayControls();
+            bool exitFlag = true;
+            do
+            {
+                DisplayAvailableRooms();
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.W :
+                    {
+                        if (s_player.CurrentRoom.NorthRoom != null)
+                        {
+                            Console.WriteLine("You moved into northern room\n");
+                            s_player.CurrentRoom = s_player.CurrentRoom.NorthRoom;
+                        } else Console.WriteLine("There is no room to the north. Only a wall.");
+                        break;
+                    }
+                    case ConsoleKey.D :
+                    {
+                        if (s_player.CurrentRoom.EastRoom != null)
+                        {
+                            Console.WriteLine("You moved into eastern room\n");
+                            s_player.CurrentRoom = s_player.CurrentRoom.EastRoom;
+                        } else Console.WriteLine("There is no room to the east. Only a wall.");
+                        break;
+                    }
+                    case ConsoleKey.S :
+                    {
+                        if (s_player.CurrentRoom.SouthRoom != null)
+                        {
+                            Console.WriteLine("You moved into southern room\n");
+                            s_player.CurrentRoom = s_player.CurrentRoom.SouthRoom;
+                        } else Console.WriteLine("There is no room to the south. Only a wall.");
+                        break;
+                    }
+                    case ConsoleKey.A :
+                    {
+                        if (s_player.CurrentRoom.WestRoom != null)
+                        {
+                            Console.WriteLine("You moved into western room\n");
+                            s_player.CurrentRoom = s_player.CurrentRoom.WestRoom;
+                        } else Console.WriteLine("There is no room to the west. Only a wall.");
+                        break;
+                    }
+                    case ConsoleKey.E :
+                    {
+                        Console.WriteLine("You look around.\n");
+                        Console.WriteLine(s_player.CurrentRoom.Description);
+                        break;
+                    }
+                    case ConsoleKey.C: DisplayControls(); break;
+                    case ConsoleKey.Q : if(ConfirmQuit()) exitFlag=false; break;
+                    default:
+                    {
+                        Console.WriteLine("Invalid input");
+                        DisplayControls();
+                        break;
+                    }
+                }
+               
+            } while (exitFlag);
+        }
+
+        static World s_world = new World();
+        static Player s_player = new Player();
+
+        static void DisplayAvailableRooms()
+        {
+            Console.WriteLine("\nThere are entryways to the:");
+            if (s_player.CurrentRoom.NorthRoom != null) Console.WriteLine("North");
+            if (s_player.CurrentRoom.SouthRoom != null) Console.WriteLine("South");
+            if (s_player.CurrentRoom.EastRoom != null) Console.WriteLine("East");
+            if (s_player.CurrentRoom.WestRoom != null) Console.WriteLine("West");
+        }
+        static void DisplayControls()
+        {
+            Console.WriteLine("W to move north.");
+            Console.WriteLine("A to move west.");
+            Console.WriteLine("S to move south.");
+            Console.WriteLine("D to move east.");
+            Console.WriteLine("E to look around.");
+            Console.WriteLine("C to display controls.");
+            Console.WriteLine("Q to quit program.");
+        }
+        private static bool ConfirmQuit ()
+        {
+            return ReadBoolean("Are you sure you want to quit? (y/n) ");
+        }
+        private static bool ReadBoolean ( string message )
+        {
+            Console.Write(message);
+            Console.WriteLine();
+
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Y)
+                {
+                    Console.WriteLine("Y");
+                    return true;
+                } else if (key.Key == ConsoleKey.N)
+                {
+                    Console.WriteLine("N");
+                    return false;
+                }
+
+                Console.WriteLine("Invalid Input. Must enter Y/N.");
+            } while (true);
         }
     }
 }
--- labs/Lab2/JohnLobsinger.AdventureGame/Room.cs
+++ labs/Lab2/JohnLobsinger.AdventureGame/Room.cs
@@ -2,6 +2,9 @@
 
 namespace JohnLobsinger.AdventureGame
 {
+    /// <summary>
+    /// Represents a room
+    /// </summary>
     public class Room
     {
         /// <summary>
@@ -22,5 +25,158 @@ namespace JohnLobsinger.AdventureGame
             set { _description = value; }
         }
         private string _description;
+        /// <summary>
+        /// Reads and writes the room to the north
+        /// </summary>
+        public Room NorthRoom { get; set; }
+        /// <summary>
+        /// Determines if there is a room to the south
+        /// </summary>
+        public Room SouthRoom { get; set; }
+        /// <summary>
+        /// Determines if there is a room to the east
+        /// </summary>
+        public Room EastRoom { get; set; }
+        /// <summary>
+        /// Determines if there is a room to the west
+        /// </summary>
+        public Room WestRoom { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a set of rooms
+    /// </summary>
+    public class World
+    {
+        /// <summary>
+        /// Reads and writes the NorthWestRoom
+        /// </summary>
+        public Room NorthWestRoom { get; set; }
+        /// <summary>
+        /// Reads and writes the NorthRoom
+        /// </summary>
+        public Room NorthRoom { get; set; }
+        /// <summary>
+        /// Reads and writes the NorthEastRoom
+        /// </summary>
+        public Room NorthEastRoom { get; set; }
+        /// <summary>
+        /// Reads and writes the WestRoom
+        /// </summary>
+        public Room WestRoom { get; set; }
+        /// <summary>
+        /// Reads and writes the CenterRoom
+        /// </summary>
+        public Room CenterRoom { get; set; }
+        /// <summary>
+        /// Reads and writes the EastRoom
+        /// </summary>
+        public Room EastRoom { get; set; }
+        /// <summary>
+        /// Reads and writes the SotuhWestRoom
+        /// </summary>
+        public Room SouthWestRoom { get; set; }
+        /// <summary>
+        /// Reads and writes the SouthRoom
+        /// </summary>
+        public Room SouthRoomStart { get; set; } // This will be the starting room
+        /// <summary>
+        /// Reads and writes the SouthEastRoom
+        /// </summary>
+        public Room SouthEastRoom { get; set; }
+
+        public void Initialize()
+        {
+            // For the record I hate this.
+
+            NorthWestRoom = new Room();
+            NorthWestRoom.Name = "Barrel Room";
+            NorthWestRoom.Description = "The room is filled with barrels from floor to ceiling. It smells of brine and salted meats.";
+
+            NorthRoom = new Room();
+            NorthRoom.Name = "Furniture Room";
+            NorthRoom.Description = "The room is filled with old stools, tables, and various other furnitures for festivals.";
+
+            NorthEastRoom = new Room();
+            NorthEastRoom.Name = "Empty Room";
+            NorthEastRoom.Description = "The room is empty, there is nothing notable except a few cracks and cobwebs";
+            
+            WestRoom = new Room();
+            WestRoom.Name = "Barrel Room?";
+            WestRoom.Description = "The room is filled with barrels from floor to ceiling. The smell of blood and brine is strong.";
+
+            CenterRoom = new Room();
+            CenterRoom.Name = "Shrine Room";
+            CenterRoom.Description = "There is a mysterious shrine in the center of the room. The scratch marks seem to lead underneath it. It is too heavy to move.";
+            
+            EastRoom = new Room();
+            EastRoom.Name = "Empty Room";
+            EastRoom.Description = "The room is empty, there is nothing notable except a few cracks and cobwebs";
+            
+            SouthWestRoom = new Room();
+            SouthWestRoom.Name = "Butchers' room";
+            SouthWestRoom.Description = "The smell fo blood is thick in the air. You gaze around to see various blades and hooks coated in dried blood. This was likely used for butchering game, at least you hope";
+            
+            SouthRoomStart = new Room();
+            SouthRoomStart.Name = "Start Room";
+            SouthRoomStart.Description = "The room is mostly empty besides the ladder against the south wall and the rays of light from the surface. The scratch marks lead north.";
+            
+            SouthEastRoom = new Room();
+            SouthEastRoom.Name = "Empty Room?";
+            SouthEastRoom.Description = "The room is empty, there is nothing notable except a few cracks and cobwebs... and some strange... humming?";
+            
+            NorthWestRoom.SouthRoom = WestRoom;
+            NorthWestRoom.EastRoom = NorthRoom;
+
+            NorthRoom.SouthRoom = CenterRoom;
+            NorthRoom.EastRoom = NorthEastRoom;
+            NorthRoom.WestRoom = NorthWestRoom;
+
+            NorthEastRoom.SouthRoom = EastRoom;
+            NorthEastRoom.WestRoom = NorthRoom;
+
+            WestRoom.NorthRoom = NorthWestRoom;
+            WestRoom.SouthRoom = SouthWestRoom;
+            WestRoom.EastRoom = CenterRoom;
+
+            CenterRoom.SouthRoom = SouthRoomStart;
+            CenterRoom.NorthRoom = NorthRoom;
+            CenterRoom.EastRoom = EastRoom;
+            CenterRoom.WestRoom = WestRoom;
+
+            EastRoom.NorthRoom = NorthEastRoom;
+            EastRoom.SouthRoom = SouthEastRoom;
+            EastRoom.WestRoom = CenterRoom;
+
+            SouthWestRoom.NorthRoom = WestRoom;
+            SouthWestRoom.EastRoom = SouthRoomStart;
+
+            SouthRoomStart.NorthRoom = CenterRoom;
+            SouthRoomStart.EastRoom = SouthEastRoom;
+            SouthRoomStart.WestRoom = SouthWestRoom;
+
+            SouthEastRoom.NorthRoom = EastRoom;
+            SouthEastRoom.WestRoom = SouthRoomStart;
+        }
+        /// <summary>
+        /// Displays the game  world's setting
+        /// </summary>
+        public void DisplaySetting ()
+        {
+            Console.WriteLine("Greetings adventurer, while on a quest to track the disapearance of some villagers you arive to find it deserted");
+            Console.WriteLine("While searching for the missing villagers you find scratch marks leading to a celler in the chiefs house.");
+            Console.WriteLine("As you open the hatch and peer into a pitch black haze, you light your torch and venture down.\n");
+        }
+    }
+
+    /// <summary>
+    /// Represents a player
+    /// </summary>
+    public class Player
+    {
+        /// <summary>
+        /// Reads and writes the current room of the player
+        /// </summary>
+        public Room CurrentRoom { get; set; }
     }
 }
